5c5c369558652e5633420ae15a64e8286d6130e6
Taha hemdan <mhmtdh362@gmail.com>
2026-01-18 17:09:28 +0200

    Refactor navigation and theme toggle functionality
    Combine scroll event handling and improve theme toggle functionality.

--- main.js
+++ main.js
@@ -1,78 +1,76 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
 function myMenuFunction() {
-  const menuBtn = document.getElementById("myNavMenu");
-  const closeBtn = document.getElementById("close-btn");
+    const menuBtn = document.getElementById("myNavMenu");
 
-  if (menuBtn.className === "nav-menu") {
-    menuBtn.className += " responsive";
-    closeBtn.classList.remove("hidden"); // Show close button
-  } else {
-    menuBtn.className = "nav-menu";
-    closeBtn.classList.add("hidden"); // Hide close button
-  }
+    if (menuBtn.className === "nav-menu") {
+        menuBtn.className += " responsive";
+    } else {
+        menuBtn.className = "nav-menu";
+    }
 }
 
 // Close menu after clicking on a link
 const navLinks = document.querySelectorAll(".nav-menu a");
 
 navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    const menuBtn = document.getElementById("myNavMenu");
-    const closeBtn = document.getElementById("close-btn");
-    menuBtn.className = "nav-menu"; // Close the menu
-    closeBtn.classList.add("hidden"); // Hide close button
-  });
+    link.addEventListener("click", () => {
+        const menuBtn = document.getElementById("myNavMenu");
+        menuBtn.className = "nav-menu";
+    });
 });
 
 // Close menu when clicking outside
 document.addEventListener("click", (event) => {
-  const menu = document.getElementById("myNavMenu");
-  const menuToggle = document.getElementById("menu-toggle");
-  const closeBtn = document.getElementById("close-btn");
-  if (
-    !menu.contains(event.target) &&
-    !menuToggle.contains(event.target) &&
-    !closeBtn.contains(event.target)
-  ) {
-    menu.className = "nav-menu"; // Close the menu
-    closeBtn.classList.add("hidden"); // Hide close button
-  }
+    const menu = document.getElementById("myNavMenu");
+    const menuToggle = document.querySelector(".nav-menu-btn i");
+    const themeBtn = document.getElementById("theme-button");
+
+    if (
+        !menu.contains(event.target) &&
+        !menuToggle.contains(event.target) &&
+        !themeBtn.contains(event.target)
+    ) {
+        menu.className = "nav-menu";
+    }
 });
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
 window.onscroll = function () {
-  headerShadow();
+    headerShadow();
+    scrollActive(); // Combine scroll events
 };
 
 function headerShadow() {
-  const navHeader = document.getElementById("header");
+    const navHeader = document.getElementById("header");
 
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
-    navHeader.style.height = "70px";
-    navHeader.style.lineHeight = "70px";
-  } else {
-    navHeader.style.boxShadow = "none";
-    navHeader.style.height = "90px";
-    navHeader.style.lineHeight = "90px";
-  }
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navHeader.classList.add("nav-header-scrolled");
+        navHeader.style.boxShadow = "var(--shadow-1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+    } else {
+        navHeader.classList.remove("nav-header-scrolled");
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+    }
 }
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText", {
-  strings: ["Frontend", "Backend", "UI UX"],
-  loop: true,
-  typeSpeed: 100,
-  backSpeed: 80,
-  backDelay: 2000,
+    strings: ["Frontend Developer", "Backend Developer", "UI/UX Designer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000,
 });
 
-/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+/* ----- SCROLL REVEAL ANIMATION ----- */
 const sr = ScrollReveal({
-  origin: "top",
-  distance: "80px",
-  duration: 2000,
-  reset: true,
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
 });
 
 /* -- HOME -- */
@@ -89,14 +87,14 @@ sr.reveal(".project-box", { interval: 200 });
 /* -- HEADINGS -- */
 sr.reveal(".top-header", {});
 
-/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+/* ----- SCROLL REVEAL LEFT_RIGHT ANIMATION ----- */
 
 /* -- ABOUT INFO & CONTACT INFO -- */
 const srLeft = ScrollReveal({
-  origin: "left",
-  distance: "80px",
-  duration: 2000,
-  reset: true,
+    origin: "left",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
 });
 
 srLeft.reveal(".about-info", { delay: 100 });
@@ -104,10 +102,10 @@ srLeft.reveal(".contact-info", { delay: 100 });
 
 /* -- ABOUT SKILLS & FORM BOX -- */
 const srRight = ScrollReveal({
-  origin: "right",
-  distance: "80px",
-  duration: 2000,
-  reset: true,
+    origin: "right",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
 });
 
 srRight.reveal(".skills-box", { delay: 100 });
@@ -117,37 +115,50 @@ srRight.reveal(".form-control", { delay: 100 });
 const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
-  const scrollY = window.scrollY;
+    const scrollY = window.scrollY;
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 50,
-      sectionId = current.getAttribute("id");
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute("id");
 
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav-menu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(".nav-menu a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
-    }
-  });
+        const navLink = document.querySelector(".nav-menu a[href*=" + sectionId + "]");
+        if (navLink) {
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                navLink.classList.add("active-link");
+            } else {
+                navLink.classList.remove("active-link");
+            }
+        }
+    });
 }
 
-// Debounce scrollActive for better performance
-function debounce(func, wait = 20) {
-  let timeout;
-  return function (...args) {
-    const later = () => {
-      clearTimeout(timeout);
-      func(...args);
-    };
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-  };
+/* ----- DARK / LIGHT THEME ----- */
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'light' : 'dark'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-sun' : 'uil-moon'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'light' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedIcon === 'uil-sun' ? 'add' : 'remove'](iconTheme)
 }
 
-window.addEventListener("scroll", debounce(scrollActive, 50));
-
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the light / icon theme
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
